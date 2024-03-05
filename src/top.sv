a95f981749b7387e4de7c45a619e11b8250bc2a5
gabejessil <80356765+gabejessil@users.noreply.github.com>
2024-03-05 08:52:08 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -4,7 +4,7 @@
 //_\SV
    // Include Tiny Tapeout Lab.
    // Included URL: "https://raw.githubusercontent.com/os-fpga/Virtual-FPGA-Lab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlv_lib/tiny_tapeout_lib.tlv"// Included URL: "https://raw.githubusercontent.com/os-fpga/Virtual-FPGA-Lab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlv_lib/fpga_includes.tlv"
-//_\source top.tlv 103
+//_\source top.tlv 129
 
 // +++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE+++++++
 // +++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE++++++++++++++INSERT CODE+++++++
@@ -110,31 +110,17 @@ logic FpgaPins_Fpga_TIME_clk_disp_a0,
       FpgaPins_Fpga_TIME_clk_disp_a1,
       FpgaPins_Fpga_TIME_clk_disp_a2;
 
-// For /fpga_pins/fpga|time$cnt.
-logic [20:0] FpgaPins_Fpga_TIME_cnt_a0,
-             FpgaPins_Fpga_TIME_cnt_a1;
-
 // For /fpga_pins/fpga|time$digit.
 logic [3:0] FpgaPins_Fpga_TIME_digit_a0;
 
 // For /fpga_pins/fpga|time$ones.
-logic [3:0] FpgaPins_Fpga_TIME_ones_a0,
-            FpgaPins_Fpga_TIME_ones_a1;
+logic [3:0] FpgaPins_Fpga_TIME_ones_a0;
 
 // For /fpga_pins/fpga|time$reset.
 logic FpgaPins_Fpga_TIME_reset_a0;
 
-// For /fpga_pins/fpga|time$sec_cnt.
-logic [3:0] FpgaPins_Fpga_TIME_sec_cnt_a0,
-            FpgaPins_Fpga_TIME_sec_cnt_a1;
-
 // For /fpga_pins/fpga|time$tens.
-logic [3:0] FpgaPins_Fpga_TIME_tens_a0,
-            FpgaPins_Fpga_TIME_tens_a1;
-
-// For /fpga_pins/fpga|time$time_clk.
-logic FpgaPins_Fpga_TIME_time_clk_a0,
-      FpgaPins_Fpga_TIME_time_clk_a1;
+logic [3:0] FpgaPins_Fpga_TIME_tens_a0;
 
 
 
@@ -156,21 +142,6 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
             // Staging of $clk_disp.
             always_ff @(posedge clk) FpgaPins_Fpga_TIME_clk_disp_a1 <= FpgaPins_Fpga_TIME_clk_disp_a0;
             always_ff @(posedge clk) FpgaPins_Fpga_TIME_clk_disp_a2 <= FpgaPins_Fpga_TIME_clk_disp_a1;
-
-            // Staging of $cnt.
-            always_ff @(posedge clk) FpgaPins_Fpga_TIME_cnt_a1[20:0] <= FpgaPins_Fpga_TIME_cnt_a0[20:0];
-
-            // Staging of $ones.
-            always_ff @(posedge clk) FpgaPins_Fpga_TIME_ones_a1[3:0] <= FpgaPins_Fpga_TIME_ones_a0[3:0];
-
-            // Staging of $sec_cnt.
-            always_ff @(posedge clk) FpgaPins_Fpga_TIME_sec_cnt_a1[3:0] <= FpgaPins_Fpga_TIME_sec_cnt_a0[3:0];
-
-            // Staging of $tens.
-            always_ff @(posedge clk) FpgaPins_Fpga_TIME_tens_a1[3:0] <= FpgaPins_Fpga_TIME_tens_a0[3:0];
-
-            // Staging of $time_clk.
-            always_ff @(posedge clk) FpgaPins_Fpga_TIME_time_clk_a1 <= FpgaPins_Fpga_TIME_time_clk_a0;
 
 
 
@@ -226,20 +197,14 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                assign \///@0$btn = FpgaPins_Fpga_TIME_btn_a0;
                (* keep *) logic  \///@0$clk_disp ;
                assign \///@0$clk_disp = FpgaPins_Fpga_TIME_clk_disp_a0;
-               (* keep *) logic [20:0] \///@0$cnt ;
-               assign \///@0$cnt = FpgaPins_Fpga_TIME_cnt_a0;
                (* keep *) logic [3:0] \///@0$digit ;
                assign \///@0$digit = FpgaPins_Fpga_TIME_digit_a0;
                (* keep *) logic [3:0] \///@0$ones ;
                assign \///@0$ones = FpgaPins_Fpga_TIME_ones_a0;
                (* keep *) logic  \///@0$reset ;
                assign \///@0$reset = FpgaPins_Fpga_TIME_reset_a0;
-               (* keep *) logic [3:0] \///@0$sec_cnt ;
-               assign \///@0$sec_cnt = FpgaPins_Fpga_TIME_sec_cnt_a0;
                (* keep *) logic [3:0] \///@0$tens ;
                assign \///@0$tens = FpgaPins_Fpga_TIME_tens_a0;
-               (* keep *) logic  \///@0$time_clk ;
-               assign \///@0$time_clk = FpgaPins_Fpga_TIME_time_clk_a0;
             end
          end
       end
@@ -259,7 +224,7 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
 //_\TLV
    /* verilator lint_off UNOPTFLAT */
    // Connect Tiny Tapeout I/Os to Virtual FPGA Lab.
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 76   // Instantiated from top.tlv, 175 as: m5+tt_connections()
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 76   // Instantiated from top.tlv, 201 as: m5+tt_connections()
       assign L0_slideswitch_a0[7:0] = ui_in;
       assign L0_sseg_segment_n_a0[6:0] = ~ uo_out[6:0];
       assign L0_sseg_decimal_point_n_a0 = ~ uo_out[7];
@@ -267,7 +232,7 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
    //_\end_source
 
    // Instantiate the Virtual FPGA Lab.
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 307   // Instantiated from top.tlv, 178 as: m5+board(/top, /fpga, 7, $, , my_design)
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 307   // Instantiated from top.tlv, 204 as: m5+board(/top, /fpga, 7, $, , my_design)
       
       //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 355   // Instantiated from /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv, 309 as: m4+thanks(m5__l(309)m5_eval(m5_get(BOARD_THANKS_ARGS)))
          //_/thanks
@@ -291,40 +256,18 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                      // divides clock for better switching on dual seven-segment
                      assign FpgaPins_Fpga_TIME_clk_disp_a0 = FpgaPins_Fpga_TIME_clk_disp_a2 ? 1'b0 : FpgaPins_Fpga_TIME_clk_disp_a2 + 1'b1;
             
-                     assign FpgaPins_Fpga_TIME_cnt_a0[20:0] =
-                        FpgaPins_Fpga_TIME_reset_a0 ? 0 :
-                        FpgaPins_Fpga_TIME_time_clk_a1 ? 0 :
-                        (1 + FpgaPins_Fpga_TIME_cnt_a1);
             
-                     assign FpgaPins_Fpga_TIME_time_clk_a0 = (FpgaPins_Fpga_TIME_cnt_a0 == 21'b111101000010010000000);
-            
-                     // 100 in binary 1100100
-                     assign FpgaPins_Fpga_TIME_sec_cnt_a0[3:0] =
-                        FpgaPins_Fpga_TIME_reset_a0 ? 0 :
-                        (FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) ? 4'd0 :
-                        FpgaPins_Fpga_TIME_time_clk_a1 ? (1 + FpgaPins_Fpga_TIME_sec_cnt_a1):
-                        FpgaPins_Fpga_TIME_sec_cnt_a1;
-            
-                     assign FpgaPins_Fpga_TIME_ones_a0[3:0] =
-                        FpgaPins_Fpga_TIME_reset_a0 ? 4'd9 :
-                        (FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 != 4'd0)) ? (FpgaPins_Fpga_TIME_ones_a1 - 4'd1) :
-                        (FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 == 4'd0)) ? 4'd9 :
-                        FpgaPins_Fpga_TIME_ones_a1;
-            
-                     assign FpgaPins_Fpga_TIME_tens_a0[3:0] =
-                        FpgaPins_Fpga_TIME_reset_a0 ? 4'd9 :
-                        ((FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 != 4'd0)) ? (FpgaPins_Fpga_TIME_tens_a1 - 4'd1) :
-                        ((FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0)) ? 4'd9 :
-                        FpgaPins_Fpga_TIME_tens_a1;
+                     assign FpgaPins_Fpga_TIME_tens_a0[3:0] = 4'd1;
+                     assign FpgaPins_Fpga_TIME_ones_a0[3:0] = 4'd3;
             
                      assign FpgaPins_Fpga_TIME_digit_a0[3:0] = FpgaPins_Fpga_TIME_clk_disp_a0 ? FpgaPins_Fpga_TIME_tens_a0 : FpgaPins_Fpga_TIME_ones_a0;
             
                      // decodes either tens or ones place to the seven-segments
                      assign uo_out[7:0] = {FpgaPins_Fpga_TIME_clk_disp_a0,
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd0) ? 7'b0111111 :
-                        (FpgaPins_Fpga_TIME_digit_a0 == 4'd1) ? 7'b0000110 :
+                        ((FpgaPins_Fpga_TIME_digit_a0 == 4'd1) && (FpgaPins_Fpga_TIME_btn_a0 == 4'd0 || FpgaPins_Fpga_TIME_btn_a0 == 4'd4)) ? 7'b0000110 :
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd2) ? 7'b1011011 :
-                        (FpgaPins_Fpga_TIME_digit_a0 == 4'd3) ? 7'b1001111 :
+                        ((FpgaPins_Fpga_TIME_digit_a0 == 4'd3) && (FpgaPins_Fpga_TIME_btn_a0 == 4'd1 || FpgaPins_Fpga_TIME_btn_a0 == 4'd2)) ? 7'b1001111 :
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd4) ? 7'b1100110 :
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd5) ? 7'b1101101 :
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd6) ? 7'b1111101 :
@@ -332,6 +275,54 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd8) ? 7'b1111111 :
                         (FpgaPins_Fpga_TIME_digit_a0 == 4'd9) ? 7'b1101111 :
                                          7'b0000000};
+            
+            
+                     /* COMMENTED TO WORK ON BUTTON INPUT
+                     // divides clock for better switching on dual seven-segment
+                     $clk_disp = >>2$clk_disp ? 1'b0 : >>2$clk_disp + 1'b1;
+            
+                     $cnt[20:0] =
+                        $reset ? 0 :
+                        >>1$time_clk ? 0 :
+                        (1 + >>1$cnt);
+            
+                     $time_clk = ($cnt == 21'b111101000010010000000);
+            
+                     // 100 in binary 1100100
+                     $sec_cnt[3:0] =
+                        $reset ? 0 :
+                        (>>1$sec_cnt == 4'd10) ? 4'd0 :
+                        >>1$time_clk ? (1 + >>1$sec_cnt):
+                        >>1$sec_cnt;
+            
+                     $ones[3:0] =
+                        $reset ? 4'd9 :
+                        (>>1$time_clk && (>>1$ones != 4'd0)) ? (>>1$ones - 4'd1) :
+                        (>>1$time_clk && (>>1$ones == 4'd0)) ? 4'd9 :
+                        >>1$ones;
+            
+                     $tens[3:0] =
+                        $reset ? 4'd9 :
+                        ((>>1$sec_cnt == 4'd10) && (>>1$tens != 4'd0)) ? (>>1$tens - 4'd1) :
+                        ((>>1$sec_cnt == 4'd10) && (>>1$tens == 4'd0)) ? 4'd9 :
+                        >>1$tens;
+            
+                     $digit[3:0] = $clk_disp ? $tens : $ones;
+            
+                     // decodes either tens or ones place to the seven-segments
+                     *uo_out[7:0] = {$clk_disp,
+                        ($digit == 4'd0) ? 7'b0111111 :
+                        ($digit == 4'd1) ? 7'b0000110 :
+                        ($digit == 4'd2) ? 7'b1011011 :
+                        ($digit == 4'd3) ? 7'b1001111 :
+                        ($digit == 4'd4) ? 7'b1100110 :
+                        ($digit == 4'd5) ? 7'b1101101 :
+                        ($digit == 4'd6) ? 7'b1111101 :
+                        ($digit == 4'd7) ? 7'b0000111 :
+                        ($digit == 4'd8) ? 7'b1111111 :
+                        ($digit == 4'd9) ? 7'b1101111 :
+                                         7'b0000000};
+                     */
             
             
                // Connect Tiny Tapeout outputs. Note that uio_ outputs are not available in the Tiny-Tapeout-3-based FPGA boards.
@@ -373,7 +364,7 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
       
    //_\end_source
    // Label the switch inputs [0..7] (1..8 on the physical switch panel) (top-to-bottom).
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 82   // Instantiated from top.tlv, 180 as: m5+tt_input_labels_viz(⌈"UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED"⌉)
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 82   // Instantiated from top.tlv, 206 as: m5+tt_input_labels_viz(⌈"UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED"⌉)
       for (input_label = 0; input_label <= 7; input_label++) begin : L1_InputLabel //_/input_label
          
       end
