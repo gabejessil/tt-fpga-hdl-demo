2ca14fb7c6a046fb0a9a0246d58a05e32a0ac908
gabejessil <80356765+gabejessil@users.noreply.github.com>
2024-03-05 11:45:54 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -376,8 +376,8 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                      //left disp (currently showing right????)
                      assign FpgaPins_Fpga_TIME_ones_a0[3:0] =
                         FpgaPins_Fpga_TIME_reset_a0 ? 4'd10 :
-                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 && !FpgaPins_Fpga_TIME_pause_a0 ? 4'd10 :
-                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd4) || FpgaPins_Fpga_TIME_pause_a0 ? FpgaPins_Fpga_TIME_ones_a1 :
+                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 ? 4'd10 :
+                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd4) ? FpgaPins_Fpga_TIME_ones_a1 :
                         (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) ? 4'd0 :
                         FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 != 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? (FpgaPins_Fpga_TIME_ones_a1 - 4'd1) :
                         FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? 4'd9 :
@@ -385,8 +385,8 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                      //right disp
                      assign FpgaPins_Fpga_TIME_tens_a0[3:0] =
                         FpgaPins_Fpga_TIME_reset_a0 ? 4'd10 :
-                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 && !FpgaPins_Fpga_TIME_pause_a0 ? 4'd10 :
-                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd4) || FpgaPins_Fpga_TIME_pause_a0 ? FpgaPins_Fpga_TIME_tens_a1 :
+                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 ? 4'd10 :
+                        (FpgaPins_Fpga_TIME_btn_a0 == 4'd4) ? FpgaPins_Fpga_TIME_tens_a1 :
                         (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) ? 4'd0 :
                         (FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 != 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? (FpgaPins_Fpga_TIME_tens_a1 - 4'd1) :
                         (FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? 4'd9 :
