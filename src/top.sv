4fed4deae52b19d0c18ba40239f7053f82e4cf2b
gabejessil <80356765+gabejessil@users.noreply.github.com>
2024-03-05 15:22:20 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -397,7 +397,7 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                      assign FpgaPins_Fpga_TIME_ones_a0[3:0] =
                         FpgaPins_Fpga_TIME_reset_a0 ? 4'd10 :
                         (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 ? 4'd10 :
-                        FpgaPins_Fpga_TIME_p1_sub_a0 || FpgaPins_Fpga_TIME_p2_sub_a0 ? FpgaPins_Fpga_TIME_ones_a1 :
+                        //$p1_sub || $p2_sub ? >>1$ones :
                         (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) ? 4'd0 :
                         FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 != 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? (FpgaPins_Fpga_TIME_ones_a1 - 4'd1) :
                         FpgaPins_Fpga_TIME_time_clk_a1 && (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? 4'd9 :
@@ -406,7 +406,7 @@ logic FpgaPins_Fpga_TIME_time_clk_a0,
                      assign FpgaPins_Fpga_TIME_tens_a0[3:0] =
                         FpgaPins_Fpga_TIME_reset_a0 ? 4'd10 :
                         (FpgaPins_Fpga_TIME_btn_a0 == 4'd0) && !FpgaPins_Fpga_TIME_start_a0 ? 4'd10 :
-                        FpgaPins_Fpga_TIME_p1_sub_a0 || FpgaPins_Fpga_TIME_p2_sub_a0 ? FpgaPins_Fpga_TIME_tens_a1 :
+                        //$p1_sub || $p2_sub ? >>1$tens :
                         (FpgaPins_Fpga_TIME_ones_a1 == 4'd0) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) ? 4'd0 :
                         (FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 != 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? (FpgaPins_Fpga_TIME_tens_a1 - 4'd1) :
                         (FpgaPins_Fpga_TIME_sec_cnt_a1 == 4'd10) && (FpgaPins_Fpga_TIME_tens_a1 == 4'd0) && FpgaPins_Fpga_TIME_start_a0 ? 4'd9 :
